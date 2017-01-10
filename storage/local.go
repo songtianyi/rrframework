58634cd57ef2771fe7d724c166f5f3b3f41603b3
songtianyi <songtianyi630@163.com>
2017-01-10 09:55:16 +0000

    storage add LocalDiskStorage

---
+++ storage/local.go
@@ -0,0 +1,44 @@
+package rrstorage
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+// local disk storage
+type LocalDiskStorage struct {
+	Dir string // the directory where to save binary
+}
+
+// Create a LocalDiskStorage instance
+func CreateLocalDiskStorage(dir string) StorageWrapper {
+	// check dir
+	s := &LocalDiskStorage{
+		Dir: strings.TrimSuffix(dir, "/"),
+	}
+	return s
+}
+
+// Do save binary
+func (s *LocalDiskStorage) Save(data []byte, filename string) error {
+
+	//open a file for writing
+	file, err := os.Create(s.Dir + "/" + filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	if _, err := file.Write(data); err != nil {
+		return err 
+	}
+	return nil
+}
+
+func (s *LocalDiskStorage) Fetch(filename string) ([]byte, error) {
+	b, err := ioutil.ReadFile(s.Dir + "/" + filename)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
